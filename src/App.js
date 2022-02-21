d30559956a36b577c06c5405b5e9180945e04983
Martin <esim_player@hotmail.com>
2022-02-21 20:16:30 +0100

    change

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Header from './components/Header';
 import Hero from './components/Hero'
 import Items from './components/Items';
+import Contact from './components/Contact'
 function App() {
   return (
     <>
     <Header></Header>
     <Hero></Hero>
     <Items></Items>
+    <Contact></Contact>
     </>
   );
 }
